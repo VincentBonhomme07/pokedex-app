9e17e66a4c5c21ce0351bdc7dcd5966e5eb33584
VinsElBins <vincent.bonhomme07@gmail.com>
2024-10-02 15:28:11 +0200

    Update App to display all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Pokedex from './components/Pokedex';
+import PcBox from './components/PcBox';
+import TrainerCard from './components/TrainerCard';
+import NavigationButtons from './components/NavigationButtons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Routes encapsulées dans Routes */}
+        <Routes>
+          <Route path="/" element={<Pokedex />} />
+          <Route path="/pokedex" element={<Pokedex />} />
+          <Route path="/pc-box" element={<PcBox />} />
+          <Route path="/trainer-card" element={<TrainerCard />} />
+          {/* Redirection pour les routes inconnues */}
+          <Route path="*" element={<Navigate to="/pokedex" />} />
+        </Routes>
+        <NavigationButtons />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
